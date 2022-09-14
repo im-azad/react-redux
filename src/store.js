63ad7d87139e1d07d630e7e1708003d8ab97c80a
Abul Kalam Azad <me.azad99@gmail.com>
2022-09-14 17:51:43 +0600

    todo store created.

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
-import todoReducer from './components/services/reducers/todoReducer';
 import thunk from 'redux-thunk';
+import todoReducer from './components/services/reducers/todoReducer';
 // import counterReducer from './components/services/reducers/counterReducer';
 const store = createStore(todoReducer, applyMiddleware(thunk));
-
 export default store;
